a60188af3ac1fc8bc0e60201d076e585c8d5a6b7
Amir Zamani <azadkuh@live.com>
2016-02-08 11:05:30 +0330

    update cmus.py to more pythonic way

--- plugin/cmus.py
+++ plugin/cmus.py
@@ -12,30 +12,30 @@ except ImportError:
     pass
 
 # make an equivalent for cmus-remote call
-def cmusAction(verb):
-    if verb:
-        if   verb == 'prev'   : return ['--prev',  '-Q']
-        elif verb == 'next'   : return ['--next',  '-Q']
-        elif verb == 'pause'  : return ['--pause', '-Q']
-        elif verb == 'play'   : return ['--play',  '-Q']
-        elif verb == 'stop'   : return ['--stop',  '-Q']
-        elif verb == 'curr'   : return ['-Q']
-        elif verb == 'all'    : return ['--raw', r'set aaa_mode=all']
-        elif verb == 'artist' : return ['--raw', r'set aaa_mode=artist']
-        elif verb == 'album'  : return ['--raw', r'set aaa_mode=album']
+CMUS_DICTIONARY = {
+        'curr':   ['-Q'],
+        'prev':   ['--prev',  '-Q'],
+        'next':   ['--next',  '-Q'],
+        'pause':  ['--pause', '-Q'],
+        'play':   ['--play',  '-Q'],
+        'stop':   ['--stop',  '-Q'],
+        'all':    ['--raw',   r'set aaa_mode=all'],
+        'artist': ['--raw',   r'set aaa_mode=artist'],
+        'album':  ['--raw',   r'set aaa_mode=album']
+        }
 
-    return None
-
-def doneFor(verb):
+def whatHasBeenDone(verb):
     if verb:
-        if verb == 'prev' or verb == 'next' or verb == 'play' or verb == 'curr':
+        if verb == 'curr' or verb == 'play':
             return 'playing'
+        elif verb == 'prev' or verb == 'next':
+            return 'play'
         elif verb == 'pause':
             return 'paused'
         elif verb == 'stop':
             return 'stopped'
         elif verb == 'all' or verb == 'artist' or verb == 'album':
-            print('new options has been set')
+            print('new options has been sent')
             return None
 
     return 'info'
@@ -47,62 +47,67 @@ def toInt(s):
         return 0
 
 def visualizeIfUnicode(s):
-    return pyfribidi.log2vis(s, base_direction=pyfribidi.ON)
-
-# runs a command and returns an line iterator for stdout
-def runCommand(command):
-    p = subprocess.Popen(command, stdout=subprocess.PIPE)
-    return iter(p.stdout.readline, b'')
+    return pyfribidi.log2vis(s, base_direction=pyfribidi.ON
+            ).decode('utf-8')
 
 def parseCmusStdout(arguments):
-    tags    = {'artist':'', 'album':'', 'title':'', 'track':'', 'date':''}
-    command = ['cmus-remote'] + arguments
-    proc    = subprocess.Popen(command, stdout=subprocess.PIPE)
+    proc    = subprocess.Popen(arguments, stdout=subprocess.PIPE)
     for line in iter(proc.stdout.readline, b''):
         match = re.match(r'^tag artist\s+(.*)\s+$', line, re.M|re.I)
         if match:
             tags['artist'] = match.group(1)
+            continue
 
         match = re.match(r'^tag album\s+(.*)\s+$', line, re.M|re.I)
         if match:
             tags['album'] = match.group(1)
+            continue
 
         match = re.match(r'^tag title\s+(.*)\s+$', line, re.M|re.I)
         if match:
             tags['title'] = match.group(1)
+            continue
 
         match = re.match(r'^tag date\s+(.*)\s+$', line, re.M|re.I)
         if match:
             dateNo = toInt(match.group(1))
-            if dateNo > 0:
+            if dateNo > 0: #append date only if reported > 0
                 tags['date']  = ' (@{})'.format(dateNo)
+            continue
 
         match = re.match(r'^tag tracknumber\s+(.*)\s+$', line, re.M|re.I)
         if match:
             trackId = toInt(match.group(1))
-            if trackId > 0:
+            if trackId > 0: #prepend track number only if > 0
                 tags['track'] = '{}) '.format(trackId)
-
-    done = doneFor(userAction)
-    if done is not None:
-        if hasFribidi:
-            tags['artist'] = visualizeIfUnicode(tags['artist']).decode('utf-8')
-            tags['album']  = visualizeIfUnicode(tags['album']).decode('utf-8')
-            tags['title']  = visualizeIfUnicode(tags['title']).decode('utf-8')
-
-        statusMessage = u'\u266a {}: {t[artist]} '.format(
-                doneFor(userAction), t=tags
-                )
-        if tags['album']:
-            statusMessage += u' \u266c  {t[album]}{t[date]} '.format(t=tags)
-
-        statusMessage += u' \u266b  {t[track]}{t[title]}'.format(t=tags)
-
-        print(statusMessage)
+            continue
 
 
+def makeStatusMessage(done):
+    if done is None:
+        return
+
+    if hasFribidi:
+        tags['artist'] = visualizeIfUnicode(tags['artist'])
+        tags['album']  = visualizeIfUnicode(tags['album'])
+        tags['title']  = visualizeIfUnicode(tags['title'])
+
+    statusMessage = u'\u266a {}: {t[artist]} '.format(done, t=tags)
+    if tags['album']:
+        statusMessage += u' \u266c  {t[album]}{t[date]} '.format(t=tags)
+
+    statusMessage += u' \u266b  {t[track]}{t[title]}'.format(t=tags)
+
+    print(statusMessage)
+
+
+#------------------------------------------------------------------------------
+
+tags       = {'artist':'', 'album':'', 'title':'', 'track':'', 'date':''}
 userAction = sys.argv[1]
-args       = cmusAction(userAction)
-if args is not None and len(args) > 0:
-    parseCmusStdout(args)
+isVimMode  = sys.argv[0] == 'vim_mode'
+cmusArgs   = CMUS_DICTIONARY.get(userAction, None)
+if cmusArgs is not None and len(cmusArgs) > 0:
+    parseCmusStdout(['cmus-remote'] + cmusArgs)
+    makeStatusMessage(whatHasBeenDone(userAction))
 
